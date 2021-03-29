7dbda4fe649f483515630d818c524ad7d29530fa
Jessy Williams <jessy@jessywilliams.com>
2021-03-29 12:54:54 -0400

    Loads credentials from auth.txt

--- src/swipe.php
+++ src/swipe.php
@@ -2,15 +2,29 @@
 
 require_once(__DIR__ . "/php/card-reader.php");
 
-$customer_id = trim(array_shift(array_filter([$_GET['customerId'], $_GET['customer_id']])));
-$workstation_id = trim(array_shift(array_filter([$_GET['workstationId'], $_GET['workstation_id']])));
-if (!$customer_id || !$workstation_id) {
-    http_response_code(422);
+function abort($code)
+{
+    http_response_code($code);
     die();
 }
-$swipe = swipe_card('gdp', 'Gryph.2020', $customer_id, $workstation_id, $access_token);
+
+$customer_id = trim(array_shift(array_filter([$_GET['customerId'], $_GET['customer_id']])));
+$workstation_id = trim(array_shift(array_filter([$_GET['workstationId'], $_GET['workstation_id']])));
+if (!$customer_id || !$workstation_id) abort(422);
+try {
+    if ($fh = fopen(__DIR__ . '/data/auth.txt', 'r')) {
+        $username = trim(fgets($fh));
+        $password = fgets($fh);
+    } else {
+        abort(422);
+    }
+} catch (Exception $ignored) {
+    abort(422);
+}
+
+$swipe = swipe_card($username, $password, $customer_id, $workstation_id, $access_token);
 if (!$swipe) {
-    http_response_code(400);
+    abort(400);
 }
 header('Content-Type: application/json');
 echo json_encode($swipe);
