a8863965f2b140c759b3e4982de971ec702434b7
Jessy Williams <jessy@jessywilliams.com>
2021-03-31 00:52:59 -0400

    Fixes placeholder missing image

--- src/photo.php
+++ src/photo.php
@@ -1,10 +1,13 @@
 <?php
 
-$img_filename = array_shift(array_keys($_GET));
-$img_filename = isset($img_filename) ? $img_filename : 'missing';
-$img_filename = ltrim($img_filename, '0');
+ob_start();
+$img_filename = ltrim(array_shift(array_keys($_GET)), '0');
 $img_path = "img/photo/{$img_filename}.png";
+if (!is_file($img_path)) {
+    $img_path = "img/photo/missing.png";
+}
 $fp = fopen($img_path, 'rb');
+ob_end_clean();
 header("Content-Type: image/png");
 header("Content-Length: " . filesize($img_path));
 fpassthru($fp);
